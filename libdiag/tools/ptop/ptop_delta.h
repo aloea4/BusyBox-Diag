c9624e2244d560611d1c705e62f9a6f5359e250a
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:34:50 +0800

    Update ptop_delta.h

--- libdiag/tools/ptop/ptop_delta.h
+++ libdiag/tools/ptop/ptop_delta.h
@@ -1,1 +1,27 @@
+#ifndef PTOP_DELTA_H
+#define PTOP_DELTA_H
 
+#include "ptop_snapshot.h"
+
+typedef struct {
+    pid_t pid;
+    double cpu_percent;
+    unsigned long rss_kb;
+    char state;
+    char comm[64];
+    pid_t ppid;
+} ptop_proc_view_t;
+
+typedef struct {
+    ptop_proc_view_t *items;
+    size_t count;
+} ptop_delta_result_t;
+
+int ptop_delta_compute(
+    const ptop_snapshot_t *prev,
+    const ptop_snapshot_t *curr,
+    ptop_delta_result_t *out);
+
+void ptop_delta_free(ptop_delta_result_t *out);
+
+#endif
