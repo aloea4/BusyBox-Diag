7dc004c4feac4f4d9bfce5711b5cb2a0b2d4d5b4
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:44:50 +0800

    Update ptop_tree.c

--- libdiag/tools/ptop/ptop_tree.c
+++ libdiag/tools/ptop/ptop_tree.c
@@ -1,1 +1,8 @@
+#include "ptop.h"
 
+/* skeleton placeholder: tree reconstruction not implemented yet */
+int ptop_tree_build(const ptop_delta_result_t *delta)
+{
+    (void)delta;
+    return 0;
+}
