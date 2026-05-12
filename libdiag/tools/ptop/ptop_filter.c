7c298713f1d879a474bccd04962813a74e454cdc
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:43:29 +0800

    Update ptop_filter.c

--- libdiag/tools/ptop/ptop_filter.c
+++ libdiag/tools/ptop/ptop_filter.c
@@ -1,1 +1,11 @@
+#include "ptop.h"
+#include <string.h>
 
+/* skeleton: currently no filtering applied */
+int ptop_filter_apply(ptop_delta_result_t *delta,
+                      const ptop_config_t *cfg)
+{
+    (void)delta;
+    (void)cfg;
+    return 0;
+}
