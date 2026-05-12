d0bfd989f391ba848e077ac0a5725904d6c4e053
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:45:39 +0800

    Update ptop_policy.c

--- libdiag/tools/ptop/ptop_policy.c
+++ libdiag/tools/ptop/ptop_policy.c
@@ -1,1 +1,10 @@
+#include "ptop.h"
 
+/* skeleton placeholder: policy engine not implemented yet */
+int ptop_policy_apply(ptop_delta_result_t *delta,
+                      const ptop_config_t *cfg)
+{
+    (void)delta;
+    (void)cfg;
+    return 0;
+}
