5ba711fad0660e590f092367c38b224bb1dd7e4f
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:33:12 +0800

    Update ptop_snapshot.h

--- libdiag/tools/ptop/ptop_snapshot.h
+++ libdiag/tools/ptop/ptop_snapshot.h
@@ -1,1 +1,19 @@
+#ifndef PTOP_SNAPSHOT_H
+#define PTOP_SNAPSHOT_H
 
+#include <time.h>
+#include "libdiag/diag_proc.h"
+
+typedef struct {
+    diag_proc_info_t *procs;
+    size_t proc_count;
+
+    diag_cpu_snapshot_t cpu;
+
+    struct timespec ts;
+} ptop_snapshot_t;
+
+int ptop_snapshot_collect(ptop_snapshot_t *snap);
+void ptop_snapshot_free(ptop_snapshot_t *snap);
+
+#endif
