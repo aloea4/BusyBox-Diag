f8d01dee4d72b6e6da22b778daf8ef039c146a99
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-12 22:40:59 +0800

    Update ptop_signal.c

--- libdiag/tools/ptop/ptop_signal.c
+++ libdiag/tools/ptop/ptop_signal.c
@@ -1,1 +1,44 @@
+#include "ptop.h"
+#include <signal.h>
+#include <stdio.h>
 
+static volatile sig_atomic_t g_exit_flag = 0;
+
+static void handle_signal(int sig)
+{
+    (void)sig;
+    g_exit_flag = 1;
+}
+
+int ptop_should_exit(void)
+{
+    return g_exit_flag != 0;
+}
+
+void ptop_terminal_hide_cursor(void)
+{
+    /* hide cursor */
+    printf("\033[?25l");
+    fflush(stdout);
+}
+
+void ptop_terminal_restore(void)
+{
+    /* show cursor */
+    printf("\033[?25h");
+    printf("\n");
+    fflush(stdout);
+}
+
+static void atexit_restore(void)
+{
+    ptop_terminal_restore();
+}
+
+void ptop_signal_init(void)
+{
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+
+    atexit(atexit_restore);
+}
