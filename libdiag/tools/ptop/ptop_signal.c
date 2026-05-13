f564a5b70a881b65cc92f3595b41eb8bb95fe918
凌浩雲 <s1254020@gm.ncue.edu.tw>
2026-05-13 21:45:14 +0800

    Update ptop_signal.c

--- libdiag/tools/ptop/ptop_signal.c
+++ libdiag/tools/ptop/ptop_signal.c
@@ -1,8 +1,11 @@
 #include "ptop.h"
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static volatile sig_atomic_t g_exit_flag = 0;
+static volatile sig_atomic_t g_resize_flag = 0;
+static volatile sig_atomic_t g_cursor_hidden = 0;
 
 static void handle_signal(int sig)
 {
@@ -10,9 +13,25 @@ static void handle_signal(int sig)
     g_exit_flag = 1;
 }
 
+static void handle_winch(int sig)
+{
+    (void)sig;
+    g_resize_flag = 1;
+}
+
 int ptop_should_exit(void)
 {
     return g_exit_flag != 0;
+}
+
+int ptop_terminal_resized(void)
+{
+    return g_resize_flag != 0;
+}
+
+void ptop_clear_resize_flag(void)
+{
+    g_resize_flag = 0;
 }
 
 void ptop_terminal_hide_cursor(void)
@@ -20,14 +39,19 @@ void ptop_terminal_hide_cursor(void)
     /* hide cursor */
     printf("\033[?25l");
     fflush(stdout);
+    g_cursor_hidden = 1;
 }
 
 void ptop_terminal_restore(void)
 {
+    if (!g_cursor_hidden)
+        return;
+
     /* show cursor */
     printf("\033[?25h");
     printf("\n");
     fflush(stdout);
+    g_cursor_hidden = 0;
 }
 
 static void atexit_restore(void)
@@ -39,6 +63,7 @@ void ptop_signal_init(void)
 {
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
+    signal(SIGWINCH, handle_winch);
 
     atexit(atexit_restore);
 }
